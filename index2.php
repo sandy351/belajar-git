2764e0e15bd9c58ce5e4ea814fcbfbcecf2bdbdb
sandy351 <sandi15297@gmail.com>
2020-05-25 20:05:47 +0700

    Create index2.php

---
+++ index2.php
@@ -0,0 +1,100 @@
+<!--A Design by W3layouts
+   Author: W3layout
+   Author URL: http://w3layouts.com
+   License: Creative Commons Attribution 3.0 Unported
+   License URL: http://creativecommons.org/licenses/by/3.0/
+   -->
+<!DOCTYPE html>
+<html lang="en">
+   <head>
+      <title>Rumah Bunga Adenium Scape</title>
+    <meta name="description" content="Aplikasi Forecasting">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link rel="apple-touch-icon" href="images/umk.png">
+    <link rel="shortcut icon" href="images/umk.png">
+      <!-- Meta tags -->
+      <meta name="viewport" content="width=device-width, initial-scale=1" />
+      <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+      <meta name="keywords" content="Grass login & Sign up Form Responsive Widget, Audio and Video players, Login Form Web Template, Flat Pricing Tables, Flat Drop-Downs, Sign-Up Web Templates, Flat Web Templates, Login Sign-up Responsive Web Template, Smartphone Compatible Web Template, Free Web Designs for Nokia, Samsung, LG, Sony Ericsson, Motorola Web Design"
+         />
+      <script>
+         addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); }
+      </script>
+      <!-- Meta tags -->
+      <!-- font-awesome icons -->
+      <link href="css/fontawesome-all.min.css" rel="stylesheet" type="text/css" media="all">
+      <!-- //font-awesome icons -->
+      <!--stylesheets-->
+      <link href="css/style.css" rel='stylesheet' type='text/css' media="all">
+      <!--//style sheet end here-->
+      <link href="//fonts.googleapis.com/css?family=Barlow:300,400,500" rel="stylesheet">
+   </head>
+   <body>
+      <CENTER> <img src="images/umk.png" width="300px" /></CENTER>
+      <h1 class="header-w3ls">
+         RUMAH BUNGA ADENIUM
+      </h1>
+      <div class="art-bothside">
+         <div class="sap_tabs">
+            <div id="horizontalTab">
+               <ul class="resp-tabs-list">
+                  <li class="resp-tab-item"><span>Login</span></li>
+                  <li class="resp-tab-item"><span>About</span></li>
+               </ul>
+               <div class="clearfix"> </div>
+               <div class="resp-tabs-container">
+                  <div class="tab-1 resp-tab-content">
+                     <div class="swm-right-w3ls">
+                        <form action="otentikasi.php" method="post">
+                           <div class="main">
+                              <div class="icon-head-wthree">
+                                 <h2>Login Here</h2>
+                              </div>
+                              <div class="form-left-to-w3l">
+                                 <input type="text" name="username" placeholder="Username" required="">
+                              </div>
+                              
+                              <div class="form-right-w3ls ">
+                                 <input type="password" name="password" placeholder="Password" required="">
+                              </div>
+                              <div class="btnn">
+                                 <button type="submit">LogIn</button><br>
+                              </div>
+                           </div>
+                        </form>
+                     </div>
+                  </div>
+                  <div class="tab-1 resp-tab-content">
+                     <div class="swm-left-w3ls">
+                       <div class="copy">  
+                        <img src="images/umk.png" width="200px" />
+                        <p>Rumah Bunga Adenium Scape</p>
+                        <p>Alamat: Jl. Sultan Ageng Tirtayasa, Kalikoa, Kedawung, Cirebon, Jawa Barat 45153</p>
+                       
+                       </div>
+                     </div>
+                  </div>
+               </div>
+            </div>
+         </div>
+      </div>
+      
+      <div class="copy">
+         <p>&copy;<?php echo date("Y")?>. All Rights Reserved | Design by <a href="http://www.aplikasiphpku.wordpress.com" target="_blank">APLIKASIPHPKU</a></p>
+      </div>
+      <!--js working-->
+      <script src='js/jquery-2.2.3.min.js'></script>
+      <!--//js working-->
+      <script src="js/easyResponsiveTabs.js"></script>
+      <script>
+         $(document).ready(function () {
+         	$('#horizontalTab').easyResponsiveTabs({
+         		type: 'default', //Types: default, vertical, accordion           
+         		width: 'auto', //auto or any width like 600px
+         		fit: true // 100% fit in a container
+         	});
+         });
+      </script>
+   </body>
+</html>
